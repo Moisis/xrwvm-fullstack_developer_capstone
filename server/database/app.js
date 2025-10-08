b347d1c1c3b9bc13d0616fb1c5872adea6b75849
Moisis <moisis.george96@gmail.com>
2025-10-08 23:28:06 +0300

    js linter 5

--- server/database/app.js
+++ server/database/app.js
@@ -32,7 +32,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+  res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -77,7 +77,7 @@ app.get('/fetchDealers/:state', async (req, res) => {
   }
 });
 
-/* jshint esversion: 8 */
+
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
@@ -92,8 +92,8 @@ app.get('/fetchDealer/:id', async (req, res) => {
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort( { id: -1 } )
-  let new_id = documents[0].id+1
+  const documents = await Reviews.find().sort( { id: -1 } );
+  let new_id = documents[0].id+1;
 
   const review = new Reviews({
   "id": new_id,
