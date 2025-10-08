2f43223387ef8944598bf3d64003dddda92197a3
Moisis <moisis.george96@gmail.com>
2025-10-08 16:11:39 -0400

    fixes_linting

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
-from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,7 +27,13 @@ urlpatterns = [
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path(
+        'dealer/<int:dealer_id>/',
+        TemplateView.as_view(template_name="index.html"),
+    ),
+    path(
+        'postreview/<int:dealer_id>/',
+        TemplateView.as_view(template_name="index.html"),
+    ),
     path('', TemplateView.as_view(template_name="Home.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
